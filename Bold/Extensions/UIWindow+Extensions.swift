9f8512bc1703e21bfafcd4d843102cce9e0906be
Alexander Kovalov <alexanderkovalov@rubiconware.com>
2020-03-13 14:14:53 +0200

    Fixed memory leak

---
+++ Bold/Extensions/UIWindow+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIWindow+Extensions.swift
+//  Bold
+//
+//  Created by Alexander Kovalov on 12.03.2020.
+//  Copyright © 2020 Alexander Kovalov. All rights reserved.
+//
+
+import Foundation
